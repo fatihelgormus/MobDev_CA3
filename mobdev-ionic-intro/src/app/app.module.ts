8adeb294b24ea7e1396e52cfaa6439d23182f577
fatihelgormus <104017008+fatihelgormus@users.noreply.github.com>
2022-11-23 23:09:31 +0000

    Update app.module.ts

--- mobdev-ionic-intro/src/app/app.module.ts
+++ mobdev-ionic-intro/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
