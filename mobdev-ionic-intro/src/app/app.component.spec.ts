febbb439358452666419295881cd16176cd76d34
fatihelgormus <104017008+fatihelgormus@users.noreply.github.com>
2022-11-23 23:06:58 +0000

    Update app.component.spec.ts

--- mobdev-ionic-intro/src/app/app.component.spec.ts
+++ mobdev-ionic-intro/src/app/app.component.spec.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
