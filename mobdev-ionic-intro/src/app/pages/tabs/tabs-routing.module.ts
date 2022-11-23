e837a154414271b0aa9e9debd461858de9df42b8
fatihelgormus <2022124@student.cct.ie>
2022-11-23 02:04:12 +0000

    changing tabs tabs.routine.module.ts

--- mobdev-ionic-intro/src/app/pages/tabs/tabs-routing.module.ts
+++ mobdev-ionic-intro/src/app/pages/tabs/tabs-routing.module.ts
@@ -1,12 +1,59 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { TabsPage } from './tabs.page';
 
+	
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          }
+        ]
+      },
+      {
+        path: 'death-count',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../death-count/death.module').then( m => m.Dath-countPageModule)
+          }
+        ]
+      },
+      {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
+    redirectTo: '/tabs/films',
+    pathMatch: 'full'
   }
 ];
 
